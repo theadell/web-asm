bb35e11a81bb8f9c261d6063d5ad221c2888bdda
adel <65723807+theadell@users.noreply.github.com>
2023-12-17 14:18:03 +0100

    bind to a port

--- webserver.asm
+++ webserver.asm
@@ -5,9 +5,12 @@ format ELF64 executable
 SYS_write equ 1
 SYS_exit equ 60
 SYS_socket equ 41
+SYS_bind equ 49
 
 AF_INET equ 2
 SOCK_STREAM equ 1
+
+INADDR_ANY equ 0
 
 STDOUT equ 1
 STDERR equ 2
@@ -25,6 +28,15 @@ macro write fd,buf,count
     syscall
 }
 
+macro syscall3 number, a, b, c 
+{
+    mov rax, number
+    mov rdi, a 
+    mov rsi, b
+    mov rdx, c 
+    syscall
+}
+
 macro socket domain, type, protocol
 {
     mov rax, SYS_socket
@@ -34,6 +46,10 @@ macro socket domain, type, protocol
     syscall
 }
 
+macro bind sockfd, addr, addr_len
+{
+    syscall3 SYS_bind, sockfd, addr, addr_len
+}
 macro exit code
 {
     mov rax, SYS_exit
@@ -45,11 +61,28 @@ macro exit code
 segment readable executable
 entry main
 main:
+  ;; Startup message 
   write STDOUT, start, start_len
+
+  ;; Create an internet (ipv4) stream (tcp) socket file descriptor 
+  write STDOUT, socket_trace_msg, socket_trace_msg_len
   socket AF_INET, SOCK_STREAM, 0
+  ;; check for error 
   cmp rax, 0 
   jl error
-  mov dword [sockfd], eax 
+  write STDOUT, ok_msg, ok_msg_len
+  mov qword [sockfd], rax 
+  ;; now we need to bind the socket to an address using the bind(2) syscall 
+  ;; int bind(int socket, const struct sockaddr *address, socklen_t address_len);
+  write STDOUT, bind_trace_msg, bind_trace_msg_len
+  mov word [servaddr.sin_family], AF_INET
+  mov word [servaddr.sin_port], 14619
+  mov dword [servaddr.sin_addr], INADDR_ANY
+  bind [sockfd], servaddr.sin_family, sizeof_servaddr
+  cmp rax, 0 
+  jl error
+  write STDOUT, ok_msg, ok_msg_len
+ 
   exit 0
 
 error:
@@ -63,8 +96,30 @@ error:
 
 ;; data section
 segment readable writeable
-sockfd dd 0
-start db "hello web server!", 10
+
+sockfd dq 0
+servaddr.sin_family dw 0
+servaddr.sin_port   dw 0 
+servaddr.sin_addr   dd 0
+servaddr.sin_zero   dq 0 
+sizeof_servaddr = $ - servaddr.sin_family
+
+start db "INFO: hello web server!", 10
 start_len = $ - start
-error_msg db "ERROR!", 10
+error_msg db "INFO: ERROR!", 10
 error_msg_len = $ - error_msg
+socket_trace_msg db "INFO: Creating a socket...", 10
+socket_trace_msg_len = $ - socket_trace_msg
+bind_trace_msg db "INFO: binding server to addr...", 10
+bind_trace_msg_len = $ - bind_trace_msg
+ok_msg db "INFO: Ok!", 10
+ok_msg_len = $ - ok_msg
+
+; struct sockaddr_in {
+;     short            sin_family;   // e.g. AF_INET                // 16 bits 
+;     unsigned short   sin_port;     // e.g. htons(3490)            // 16 bits 
+;     struct in_addr   sin_addr;     // see struct in_addr, below   // 32 bits 
+;     char             sin_zero[8];  // zero this if you want to    // 64 bits 
+; };
+
+
